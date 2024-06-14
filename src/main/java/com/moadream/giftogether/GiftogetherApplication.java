7c65d561848669690124722dd9925aefb4c95d59
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-06-14 17:56:22 +0900

    config: initial config

---
+++ src/main/java/com/moadream/giftogether/GiftogetherApplication.java
@@ -0,0 +1,13 @@
+package com.moadream.giftogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moadream/giftogether/GiftogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moadream.giftogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
