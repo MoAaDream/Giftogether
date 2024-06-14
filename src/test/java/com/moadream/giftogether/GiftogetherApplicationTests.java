622900ebacc177fd02baa20ed18b1046f52eed42
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-06-15 00:17:47 +0900

    config: initial config

--- src/test/java/com/moadream/giftogether/GiftogetherApplicationTests.java
+++ src/test/java/com/moadream/giftogether/GiftogetherApplicationTests.java
@@ -1,13 +1,27 @@
 package com.moadream.giftogether;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class GiftogetherApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("")
+	void test() throws Exception {
+	    //given
+
+	    //when
+
+	    //then
+
 	}
 
 }
