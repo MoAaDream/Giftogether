4139b9cd1a0d1c6596f78e97a4722642e86ae9c3
Jade9846 <101115635+Jade9846@users.noreply.github.com>
2024-06-20 11:21:59 +0900

    fix/#10: member 엔터티에 소셜 로그인 관련 필드 추가 (#11)

--- src/main/java/com/moadream/giftogether/member/model/Member.java
+++ src/main/java/com/moadream/giftogether/member/model/Member.java
@@ -1,17 +1,26 @@
 package com.moadream.giftogether.member.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import com.moadream.giftogether.Status;
 import com.moadream.giftogether.bank.model.Bank;
 import com.moadream.giftogether.funding.model.Funding;
 import com.moadream.giftogether.global.audit.BaseTimeEntity;
 import com.moadream.giftogether.wishlist.model.WishList;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 
 @Getter
@@ -22,8 +31,14 @@ public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    //설명
+    
+    @Column(nullable =false)
+    private String socialLoginId;
+    
+    @Column(nullable =false)
+    private String socailProvider;
+    
+    
     @Column(length = 30, nullable = false)
     private String username;
 
