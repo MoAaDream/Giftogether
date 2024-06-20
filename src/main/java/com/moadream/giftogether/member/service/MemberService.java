1d93ef71992a330cc24f83b461da857697692057
Jade9846 <jadej9864@gmail.com>
2024-06-20 16:59:43 +0900

    feat: 카카오 로그인, 회원가입 구현

---
+++ src/main/java/com/moadream/giftogether/member/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.moadream.giftogether.member.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moadream.giftogether.Status;
+import com.moadream.giftogether.member.MemberRepository;
+import com.moadream.giftogether.member.model.Member;
+import com.moadream.giftogether.member.model.Role;
+
+@Service
+public class MemberService {
+
+	@Autowired
+	private MemberRepository memberRepository;
+	
+	// 회원 가입된 사용자인지 확인
+	public boolean isMemberExists(String socialLoginId, String socialProvider) {
+        Member member = memberRepository.findBySocialLoginIdAndSocailProvider(socialLoginId, socialProvider);
+        return member != null;
+    }
+
+	public void createMember(Member newMember) {
+		newMember.setStatus(Status.A);
+		newMember.setRole(Role.MEMBER);
+
+		memberRepository.save(newMember);
+		
+	}
+	
+	// 사용자 정보 수정
+	
+	
+	// 탈퇴
+}
