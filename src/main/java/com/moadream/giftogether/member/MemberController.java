098327175528e743474c46c601dab61300928209
Jade9846 <101115635+Jade9846@users.noreply.github.com>
2024-06-25 10:13:25 +0900

    feat:spring security 적용 (#37)
    * feat:spring security 적용
    
    * feat: db 설정 다시 복구
    
    * feat: security 적용

--- src/main/java/com/moadream/giftogether/member/MemberController.java
+++ src/main/java/com/moadream/giftogether/member/MemberController.java
@@ -49,7 +49,7 @@ public class MemberController {
 		model.addAttribute("location", location);
 		return "login";
 	}
-
+	
 	// 로그인 콜백
 	@GetMapping("/login/oauth2/code/kakao")
 	public String kakaoLogin(@RequestParam("code") String code, HttpSession session) throws JsonProcessingException {
@@ -61,17 +61,18 @@ public class MemberController {
 
 		// 3. 사용자 정보 받기
 		// return ResponseEntity.ok(memberService.getKakaoUserInfo(accessToken));
-		HashMap<String, Object> userInfo = kakaoService.getKakaoUserInfo(accessToken);
+		HashMap<String, Object> kakaoUserInfo = kakaoService.getKakaoUserInfo(accessToken);
 
 		// 4. 사용자 정보에서 kakaoId, nickname, profile_image 추출
-		String kakaoId = userInfo.get("id").toString();
+		String kakaoId = kakaoUserInfo.get("id").toString();
 		// 사용자 정보에서 properties 객체 추출
-	    Map<String, Object> properties = (Map<String, Object>) userInfo.get("properties");
+	    Map<String, Object> properties = (Map<String, Object>) kakaoUserInfo.get("properties");
 	    
 	    // properties 객체에서 nickname과 profile_image 추출
 	    String nickname = properties.get("nickname").toString();
 	    String profileImage = properties.get("profile_image").toString();
-
+	    log.info("==================================" + nickname);
+	    log.info("==================================" + profileImage);
 		
 		 // 5. 사용자 정보를 세션에 저장
 	    session.setAttribute("kakaoId", kakaoId);
@@ -93,16 +94,20 @@ public class MemberController {
 	    }
 	    
 	    // 홈 화면으로 리다이렉트
-	    return "redirect:/mypage";
+	    return "redirect:/home";
 		}
 
-	@GetMapping("/mypage")
+	
+	
+	
+	@GetMapping("/home")
 	public String home(Model model, HttpSession session) {
 		// 세션에서 사용자 이름 가져오기
 		String nickname = (String) session.getAttribute("nickname");
 		model.addAttribute("name", nickname);
-		return "mypage";
+		return "home";
 	}
+	
 
 	/**
 	 * 카카오 로그아웃
@@ -111,13 +116,10 @@ public class MemberController {
 	 * @throws JsonProcessingException 
 	 */
 
+
 	@GetMapping("logout")
 	public String logout(HttpSession session) throws JsonProcessingException {
-        
-		if(session == null) {
-        	return "세션이 없습니다.";
-        }
-		
+       
 		String accessToken = (String) session.getAttribute("accessToken");
 
         
@@ -128,20 +130,20 @@ public class MemberController {
         }
         session.invalidate(); // 세션 무효화
         log.info("logout 경로 도착");
-        // 세션 데이터 출력
-        /*session.getAttributeNames().asIterator()
-                .forEachRemaining(name -> log.info("session name = {}, value = {}", name, session.getAttribute(name)));
-         */
-        return "redirect:/mypage";
+        
+        return "redirect:/home";
     }
+    
+
+	
 
 	/**
 	 * 마이페이지
 	 * @return
 	 */
-	@GetMapping("/{id}")
+	@GetMapping("/member/{id}")
 	public String getUserInfo(@PathVariable("id") Long id, Model model ) {
-		System.out.println("===============" + id+"번째 회원의 마이페이지");
+		
 		Member member = memberService.getMemberInfo(id);
 		GetMemberRes getMemberRes = new GetMemberRes();
 		getMemberRes.setId(member.getId());
