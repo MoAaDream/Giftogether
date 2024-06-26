807e98909661e6d3e52110b4701301344e157196
Gaeun Kim <90741039+gganddabbiya@users.noreply.github.com>
2024-06-26 11:29:14 +0900

    feat. 제품 등록 기능 (#50)

--- src/main/java/com/moadream/giftogether/product/Repository/ProductRepository.java
+++ src/main/java/com/moadream/giftogether/product/Repository/ProductRepository.java
@@ -1,12 +1,15 @@
-package com.moadream.giftogether.funding.repository;
+package com.moadream.giftogether.product.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.moadream.giftogether.product.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	//나중에 삭제 하고 옮기기
 	Optional<Product> findByProductLink(String productLink);
+	List<Product> findByWishlistLink(String wishlistLink);
 }
--- src/main/java/com/moadream/giftogether/product/model/Product.java
+++ src/main/java/com/moadream/giftogether/product/model/Product.java
@@ -1,17 +1,28 @@
 package com.moadream.giftogether.product.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.moadream.giftogether.Status;
 import com.moadream.giftogether.funding.model.Funding;
 import com.moadream.giftogether.global.audit.BaseTimeEntity;
 import com.moadream.giftogether.wishlist.model.WishList;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -54,5 +65,6 @@ public class Product extends BaseTimeEntity {
 
     @OneToMany(mappedBy = "product")
     private List<Funding> FundingList = new ArrayList<>();
+    
 
 }
