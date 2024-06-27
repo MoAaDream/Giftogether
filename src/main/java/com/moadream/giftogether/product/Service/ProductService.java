6bf6388a2c278b49073fa5001bb8b033d960c7c7
gganddabbiya <egg8993@naver.com>
2024-06-27 17:47:34 +0900

    feat. 제품 수정, 삭제  기능

--- src/main/java/com/moadream/giftogether/product/Service/ProductService.java
+++ src/main/java/com/moadream/giftogether/product/Service/ProductService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.moadream.giftogether.DataNotFoundException;
 import com.moadream.giftogether.Status;
+import com.moadream.giftogether.member.MemberRepository;
+import com.moadream.giftogether.member.model.Member;
 import com.moadream.giftogether.product.Repository.ProductRepository;
 import com.moadream.giftogether.product.model.Product;
 import com.moadream.giftogether.wishlist.model.WishList;
@@ -21,6 +23,8 @@ public class ProductService {
     private final ProductRepository productRepository;
 	
 	private final WishListRepository wishListRepository;
+	
+	private final MemberRepository memberRepository;
 	
 	public List<Product>getList() {
 		
@@ -73,6 +77,66 @@ public class ProductService {
 		p.setWishlist(wishlist);
         productRepository.save(p);
     }
+ 
+	public void modify(String name, String description, String productImg , String optionDetail, String socialId, String productLink) {
+		Member member = findMemberBySocialId(socialId);
+		Product product = findProductByProductLink(productLink);
+		
+		checkMyProduct(product, member);
+		
+		product.setName(name);
+		product.setDescription(description);
+		product.setProductImg(productImg);
+		product.setOptionDetail(optionDetail);
+		productRepository.save(product);
+		
+	}
+	
+	private Member findMemberBySocialId(String socialId) {
+		Optional<Member> member = memberRepository.findBySocialLoginId(socialId);
+		if (member.isPresent()) {
+            return member.get();
+        } else {
+            throw new DataNotFoundException("member not found");
+        }
+	}
+	
+	private Product findProductByProductLink(String productLink) {
+		Optional<Product> product = productRepository.findByProductLink(productLink);
+		if (product.isPresent()) {
+			return product.get();
+	    } else {
+	        throw new DataNotFoundException("product not found");
+	    }
+	
+	}
+	
+
+
+	private void checkMyProduct(Product product, Member member) {
+        if(!product.getWishlist().getMember().getId().equals(member.getId()))
+            throw new DataNotFoundException("수정권한 없음");
+    }
+
+	public void delete(String productLink,String socialId) {
+		Member member = findMemberBySocialId(socialId);
+		Product product = findProductByProductLink(productLink);
+		
+		checkMyProduct(product, member);
+	   
+        if (product.getCurrentAmount() > 0) {
+            throw new IllegalArgumentException("진행중인 펀딩은 삭제할 수 없습니다");
+        }else {
+        	System.out.println(9);
+        	productRepository.deleteById(product.getId());
+        }
+	}
+
+	private Product findByLink(String productLink) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	
 
 
--- src/main/java/com/moadream/giftogether/product/controller/ProductController.java
+++ src/main/java/com/moadream/giftogether/product/controller/ProductController.java
@@ -4,6 +4,7 @@ package com.moadream.giftogether.product.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,12 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.moadream.giftogether.product.Service.ProductService;
 import com.moadream.giftogether.product.model.Product;
 import com.moadream.giftogether.product.model.ProductForm;
+import com.moadream.giftogether.product.model.ProductModifyForm;
+import com.moadream.giftogether.wishlist.model.WishList;
 import com.moadream.giftogether.wishlist.repository.WishListRepository;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,10 +38,16 @@ public class ProductController {
 	private final WishListRepository wishListRepository;
 	
 	@GetMapping("/{wishlist_link}/products")
-    public String list(Model model,  @PathVariable("wishlist_link") String wishlistLink) {
+    public String list(Model model,  @PathVariable("wishlist_link") String wishlistLink, HttpSession session) {
 		List<Product> productList = this.productService.getProductList(wishlistLink);
+		WishList wishlist = this.wishListRepository.findFirstByLink(wishlistLink).get();
         model.addAttribute("productList", productList);
         model.addAttribute("wishlistLink",wishlistLink);
+		String socialId = checkSession(session);
+		
+		if (wishlist.getMember().getSocialLoginId().equals(socialId)){
+			return "product_mylist";
+		}
 		return "product_list";
     }
 	
@@ -48,47 +59,119 @@ public class ProductController {
 	}
 	
 	@GetMapping("/{wishlist_link}/create")
-	public String createProduct(Model model, @PathVariable("wishlist_link") String wishlistLink) {
+	public String createProduct(Model model, @PathVariable("wishlist_link") String wishlistLink,  HttpSession session) {
+		WishList wishlist = this.wishListRepository.findFirstByLink(wishlistLink).get();
+		String socialId = checkSession(session);
 		model.addAttribute("productForm",new ProductForm());
 		model.addAttribute("wishlistLink",wishlistLink);
+		if (!wishlist.getMember().getSocialLoginId().equals(socialId)){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "생성 권한이 없습니다.");
+		}
 		return "product_form";
 	}
 
-	/*
-	@PostMapping("/{wishlist_link}/create")
-    public String Create(Model model,  @PathVariable("wishlist_link") String wishlistLink,
-    		@RequestParam(value="name") String name, 
-    		@RequestParam(value="description") String description, 
-    		@RequestParam(value="externalLink") String externalLink, 
-    		@RequestParam(value="uploadedImage") String productImg, 
-    		@RequestParam(value="optionDetail",required=false) String optionDetail, 
-    		@RequestParam(value="goalAmount") int goalAmount ) {
-		String productLink = UUID.randomUUID().toString().replace("-", "");
-		model.addAttribute("wishlistLink",wishlistLink);
-		this.productService.create(name, description, externalLink, productImg, optionDetail, goalAmount, productLink, wishlistLink);
-		
-        return "redirect:/{wishlist_link}/products"; // 질문 저장후 질문목록으로 이동
-    }*/
 	
 	@PostMapping("/{wishlist_link}/create")
-	public String createProduct(Model model, @PathVariable("wishlist_link") String wishlistLink, @Valid @ModelAttribute ProductForm productForm, BindingResult bindingResult) {
+	public String createProduct(Model model, @PathVariable("wishlist_link") String wishlistLink,
+			@Valid @ModelAttribute ProductForm productForm, BindingResult bindingResult,  HttpSession session) {
+		WishList wishlist = this.wishListRepository.findFirstByLink(wishlistLink).get();
+		String socialId = checkSession(session);
 		
 		String productLink = UUID.randomUUID().toString().replace("-", "");
 		model.addAttribute("wishlistLink",wishlistLink);
-
+		
+		if (!wishlist.getMember().getSocialLoginId().equals(socialId)){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "생성 권한이 없습니다.");
+		}
+		
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("productForm", productForm);
 			return "product_form";
 		}
+		
 		log.info("Creating product link: " + productForm.getUploadedImage());
 		this.productService.create(productForm.getName(), productForm.getDescription(),productForm.getExternalLink(), 
 				productForm.getUploadedImage(), productForm.getOptionDetail(), productForm.getGoalAmount(), productLink, wishlistLink);
 		
-	    return "redirect:/{wishlist_link}/products"; // 질문 저장후 질문목록으로 이동
+	    return "redirect:/{wishlist_link}/products"; 
 	}
 	
-
 	
+	@GetMapping("/products/{product_link}/modify")
+	public String modifyProduct(ProductModifyForm productModifyForm, Model model,
+			@PathVariable("product_link") String productLink, HttpSession session) {
+		Product product = this.productService.getProduct(productLink);
+		String socialId = checkSession(session);
+		model.addAttribute("product", product);
+		
+		if (!product.getWishlist().getMember().getSocialLoginId().equals(socialId)){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+		}
+		productModifyForm.setName(product.getName());
+		productModifyForm.setDescription(product.getDescription());
+		productModifyForm.setUploadedImage(product.getProductImg());
+		productModifyForm.setOptionDetail(product.getOptionDetail());
+		model.addAttribute("productModifyForm", product);
+		
+		return "product_modify";
+	}
+	
+	
+	@PostMapping("/products/{product_link}/modify")
+	public String modifyProduct(Model model, @PathVariable("product_link") String productLink,
+			@Valid @ModelAttribute ProductModifyForm productModifyForm, BindingResult bindingResult, HttpSession session) {
+		Product product = this.productService.getProduct(productLink);
+		model.addAttribute("productLink",productLink);
+		model.addAttribute("product",product);
+		model.addAttribute("productModifyForm", productModifyForm);
+		String socialId = checkSession(session);
+		
+		log.info("image = " +productModifyForm.getUploadedImage());
+		
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("productModifyForm", productModifyForm);
+			return "product_modify";
+		}
+		
+		System.out.println(productLink);
+		log.info("Creating product link: " + productModifyForm.getUploadedImage());
+		this.productService.modify(productModifyForm.getName(), productModifyForm.getDescription(), productModifyForm.getUploadedImage(), productModifyForm.getOptionDetail(), socialId, productLink);
+		
+		System.out.println(productLink);
+	    return "redirect:/products/{product_link}"; 
+	}
+
+
+	@GetMapping("/products/{product_link}/delete")
+	public String deleteProduct(@PathVariable("product_link") String productLink, HttpSession session) {
+	    try {
+	        String socialId = checkSession(session);
+	        Product product = productService.getProduct(productLink);
+	        
+	        if (!product.getWishlist().getMember().getSocialLoginId().equals(socialId)) {
+	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+	        }
+	   
+	        log.info("Deleting product link: " + productLink);
+	        this.productService.delete(productLink,socialId);
+	        
+	        // 삭제 후 해당 상품이 속한 위시리스트의 상품 목록 페이지로 리다이렉트
+	        return "redirect:/" + product.getWishlist().getLink() + "/products";
+	    } catch (Exception e) {
+	        log.error("상품 삭제 중 오류 발생: " + e.getMessage());
+	        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "상품 삭제 중 오류 발생", e);
+	    }
+	}
+
+
+	private String checkSession(HttpSession session){
+        if (session == null)
+            log.error("세션이 없습니다.");
+
+        return session.getAttribute("kakaoId").toString();
+
+    }
 	
 		        
     
