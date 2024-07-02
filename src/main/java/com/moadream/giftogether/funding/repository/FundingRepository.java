2989cf198f387f2406b07578d59e6e3ec923ee11
m-jeon <minjae77777@naver.com>
2024-07-03 08:18:00 +0900

    feat: 결제 디자인

--- src/main/java/com/moadream/giftogether/funding/repository/FundingRepository.java
+++ src/main/java/com/moadream/giftogether/funding/repository/FundingRepository.java
@@ -13,35 +13,25 @@ import com.moadream.giftogether.funding.model.Funding;
 
 @Repository
 public interface FundingRepository extends JpaRepository<Funding, Long> {
-    @Query("select o from Funding o" +
-            " left join fetch o.payment p" +
-            " left join fetch o.member m" +
-            " where o.fundingUid = :fundingUid")
-    Optional<Funding> findFundingAndPaymentAndMember(@Param("fundingUid") String fundingUid);
+	@Query("select o from Funding o" + " left join fetch o.payment p" + " left join fetch o.member m"
+			+ " where o.fundingUid = :fundingUid")
+	Optional<Funding> findFundingAndPaymentAndMember(@Param("fundingUid") String fundingUid);
 
-    @Query("select o from Funding o" +
-            " left join fetch o.payment p" +
-            " where o.fundingUid = :fundingUid")
-    Optional<Funding> findFundingAndPayment(@Param("fundingUid") String fundingUid);
-    
-    
+	@Query("select o from Funding o" + " left join fetch o.payment p" + " where o.fundingUid = :fundingUid")
+	Optional<Funding> findFundingAndPayment(@Param("fundingUid") String fundingUid);
 
-//    List<Funding> findByProductId(Long productId);
-//    List<Funding> findByMemberId(Long memberId);
-    
-    @Query("SELECT f FROM Funding f JOIN FETCH f.member m JOIN FETCH f.product p JOIN FETCH p.wishlist w JOIN FETCH w.member WHERE f.product.id = :productId")
-    List<Funding> findByProductIdWithDetails(@Param("productId") Long productId);
-    
-    @Query("SELECT f FROM Funding f JOIN FETCH f.member m JOIN FETCH f.product p JOIN FETCH p.wishlist w JOIN FETCH w.member WHERE m.socialLoginId = :socialId")
-    List<Funding> findByMemberSocialIdWithDetails(@Param("socialId") String socialId);
-    
-    Funding findByFundingUid(String fundingUid);
+	@Query("SELECT f FROM Funding f JOIN FETCH f.member m JOIN FETCH f.product p JOIN FETCH p.wishlist w JOIN FETCH w.member WHERE f.product.id = :productId")
+	List<Funding> findByProductIdWithDetails(@Param("productId") Long productId);
 
-    List<Funding> findAllByMember_Id(Long memberId);
-    
-    List<Funding> findAllByProductId(Long productId);
-    
-    List<Funding> findAllByProduct_ProductLinkAndStatus(String productLink, Status status);
+	@Query("SELECT f FROM Funding f JOIN FETCH f.member m JOIN FETCH f.product p JOIN FETCH p.wishlist w JOIN FETCH w.member WHERE m.socialLoginId = :socialId")
+	List<Funding> findByMemberSocialIdWithDetails(@Param("socialId") String socialId);
 
+	Funding findByFundingUid(String fundingUid);
+
+	List<Funding> findAllByMember_Id(Long memberId);
+
+	List<Funding> findAllByProductId(Long productId);
+
+	List<Funding> findAllByProduct_ProductLinkAndStatus(String productLink, Status status);
 
 }
--- src/main/java/com/moadream/giftogether/wishlist/service/WishListService.java
+++ src/main/java/com/moadream/giftogether/wishlist/service/WishListService.java
@@ -1,6 +1,5 @@
 package com.moadream.giftogether.wishlist.service;
 
-
 import static com.moadream.giftogether.member.exception.MemberExceptionCode.NOT_FOUND_SOCIAL_ID;
 import static com.moadream.giftogether.wishlist.exception.WishlistExceptionCode.NOT_DELETE_WISHLIST_BY_FUNDING;
 import static com.moadream.giftogether.wishlist.exception.WishlistExceptionCode.NOT_FOUND_WISHLIST;
@@ -40,143 +39,134 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class WishListService implements WishListServiceI {
 
-    private final WishListRepository wishListRepository;
-    private final MemberRepository memberRepository;
-    private final PaymentService paymentService;
-    private final ProductService productService;
-    private final ProductRepository productRepository;
+	private final WishListRepository wishListRepository;
+	private final MemberRepository memberRepository;
+	private final PaymentService paymentService;
+	private final ProductService productService;
+	private final ProductRepository productRepository;
 
+	@Transactional
+	public void createWishList(WishListForm wishListForm, String socialId) {
+		Member member = findMemberBySocialId(socialId);
 
-    @Transactional
-    public void createWishList(WishListForm wishListForm, String socialId) {
-        Member member = findMemberBySocialId(socialId);
+		WishList wishList = WishList.createWishList(wishListForm, member);
 
-        WishList wishList = WishList.createWishList(wishListForm, member);
+		wishListRepository.save(wishList);
+		log.info("SERVICE = [" + socialId + "]" + "새 위시리스트 생성");
+	}
 
-        wishListRepository.save(wishList);
-        log.info("SERVICE = [" + socialId + "]" + "새 위시리스트 생성");
-    }
+	@Override
+	@Transactional
+	public void modifyWishList(WishListForm wishListForm, String socialId, String wishlistLink) {
+		Member member = findMemberBySocialId(socialId);
+		WishList wishList = findWishListByLink(wishlistLink);
 
-    @Override
-    @Transactional
-    public void modifyWishList(WishListForm wishListForm, String socialId, String wishlistLink) {
-        Member member = findMemberBySocialId(socialId);
-        WishList wishList = findWishListByLink(wishlistLink);
+		checkMyWishList(wishList, member);
 
-        checkMyWishList(wishList, member);
+		wishList.modifyWishList(wishListForm);
+	}
 
-        wishList.modifyWishList(wishListForm);
-    }
+	@Override
+	@Transactional
+	public void deleteWishList(String socialId, String wishlistLink) {
+		Member member = findMemberBySocialId(socialId);
+		WishList wishList = findWishListByLink(wishlistLink);
 
-    @Override
-    @Transactional
-    public void deleteWishList(String socialId, String wishlistLink) {
-        Member member = findMemberBySocialId(socialId);
-        WishList wishList = findWishListByLink(wishlistLink);
+		checkMyWishList(wishList, member);
 
-        checkMyWishList(wishList, member);
+		checkFundingExist(wishList);
 
-        checkFundingExist(wishList);
+		wishListRepository.deleteById(wishList.getId());
+	}
 
-        wishListRepository.deleteById(wishList.getId());
-    }
+	@Override
+	@Transactional
+	public Page<WishlistDto> getList(String socialId, int page) {
+		Member member = findMemberBySocialId(socialId);
+		Pageable pageable = PageRequest.of(page, 5, Sort.by(Sort.Direction.ASC, "id"));
+		Page<WishList> wishListPage = wishListRepository.findAllByMember_Id(member.getId(), pageable);
+		List<WishlistDto> wishlists = wishListPage.stream().map(wishList -> new WishlistDto(wishList)).toList();
 
-    @Override
-    @Transactional
-    public Page<WishlistDto> getList(String socialId, int page) {
-        Member member = findMemberBySocialId(socialId);
-        Pageable pageable = PageRequest.of(page, 5, Sort.by(Sort.Direction.ASC, "id"));
-        Page<WishList> wishListPage = wishListRepository.findAllByMember_Id(member.getId(), pageable);
-        List<WishlistDto> wishlists = wishListPage.stream()
-                .map(wishList -> new WishlistDto(wishList))
-                .toList();
+		return new PageImpl<>(wishlists, pageable, wishListPage.getTotalElements());
+	}
 
-        return new PageImpl<>(wishlists, pageable, wishListPage.getTotalElements());
-    }
+	@Override
+	public void updateWishListStatus() {
+		List<WishList> activeWishlist = wishListRepository.findAllByStatus(Status.A);
+		LocalDateTime now = LocalDateTime.now();
+		boolean changesMade = false;
+		List<Product> productsToUpdate = new ArrayList<>();
 
-    @Override
-    public void updateWishListStatus() {
-        List<WishList> activeWishlist = wishListRepository.findAllByStatus(Status.A);
-        LocalDateTime now = LocalDateTime.now();
-        boolean changesMade = false;
-        List<Product> productsToUpdate = new ArrayList<>();
-        
-        for (WishList wishList : activeWishlist) {
-            if (wishList.getDeadline().isBefore(now)) {
-                wishList.setStatus(Status.I);    // 위시리스트의 상태를 업데이트
-                // 이 위시리스트에 연관된 모든 제품의 상태를 업데이트
-                for (Product product : wishList.getProductList()) {
-                    product.setStatus(Status.I);
-                    productsToUpdate.add(product);
-                }
-                changesMade = true;
-            }
-        }
-        // 변경 사항이 있으면 모든 위시리스트와 제품을 저장
-        if (changesMade) {
-            wishListRepository.saveAll(activeWishlist);
-            productRepository.saveAll(productsToUpdate);   
-        }
-    }
+		for (WishList wishList : activeWishlist) {
+			if (wishList.getDeadline().isBefore(now)) {
+				wishList.setStatus(Status.I); // 위시리스트의 상태를 업데이트
+				// 이 위시리스트에 연관된 모든 제품의 상태를 업데이트
+				for (Product product : wishList.getProductList()) {
+					product.setStatus(Status.I);
+					productsToUpdate.add(product);
+				}
+				changesMade = true;
+			}
+		}
+		// 변경 사항이 있으면 모든 위시리스트와 제품을 저장
+		if (changesMade) {
+			wishListRepository.saveAll(activeWishlist);
+			productRepository.saveAll(productsToUpdate);
+		}
+	}
 
-    @Override
-    public WishListForm getWishlist(String wishlistLink) {
-        WishList wishlist = findWishListByLink(wishlistLink);
+	@Override
+	public WishListForm getWishlist(String wishlistLink) {
+		WishList wishlist = findWishListByLink(wishlistLink);
 
-        return new WishListForm(wishlist);
-    }
+		return new WishListForm(wishlist);
+	}
 
-    @Override
-    public boolean checkMyPage(String socialId, String wishlistLink) {
-        Member member = findMemberBySocialId(socialId);
-        WishList wishList = findWishListByLink(wishlistLink);
+	@Override
+	public boolean checkMyPage(String socialId, String wishlistLink) {
+		Member member = findMemberBySocialId(socialId);
+		WishList wishList = findWishListByLink(wishlistLink);
 
-        if (!member.getId().equals(wishList.getMember().getId()))
-            return false;
+		if (!member.getId().equals(wishList.getMember().getId()))
+			return false;
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    @Transactional
-    public void deleteWishlistForExistFunding(String socialId, String wishlistLink) {
-        Member member = findMemberBySocialId(socialId);
-        WishList wishList = findWishListByLink(wishlistLink);
+	@Override
+	@Transactional
+	public void deleteWishlistForExistFunding(String socialId, String wishlistLink) {
+		Member member = findMemberBySocialId(socialId);
+		WishList wishList = findWishListByLink(wishlistLink);
 
-        checkMyWishList(wishList, member);
+		checkMyWishList(wishList, member);
 
-        wishList.getProductList().stream()
-                        .forEach(product -> {
-                            paymentService.productCancel(product.getProductLink(), socialId);
-                            productService.delete(product.getProductLink(), socialId);
-                        });
+		wishList.getProductList().stream().forEach(product -> {
+			paymentService.productCancel(product.getProductLink(), socialId);
+			productService.delete(product.getProductLink(), socialId);
+		});
 
-        wishListRepository.deleteById(wishList.getId());
-    }
+		wishListRepository.deleteById(wishList.getId());
+	}
 
-    private void checkMyWishList(WishList wishList, Member member) {
-        if (!wishList.getMember().getId().equals(member.getId()))
-            throw new WishListException(NOT_MY_WISHLIST);
-    }
+	private void checkMyWishList(WishList wishList, Member member) {
+		if (!wishList.getMember().getId().equals(member.getId()))
+			throw new WishListException(NOT_MY_WISHLIST);
+	}
 
+	private Member findMemberBySocialId(String socialId) {
+		return memberRepository.findBySocialLoginId(socialId)
+				.orElseThrow(() -> new MemberException(NOT_FOUND_SOCIAL_ID));
+	}
 
-    private Member findMemberBySocialId(String socialId) {
-        return memberRepository.findBySocialLoginId(socialId)
-                .orElseThrow(() -> new MemberException(NOT_FOUND_SOCIAL_ID));
-    }
+	private WishList findWishListByLink(String wishlistLink) {
+		return wishListRepository.findByLink(wishlistLink).orElseThrow(() -> new WishListException(NOT_FOUND_WISHLIST));
+	}
 
-    private WishList findWishListByLink(String wishlistLink) {
-        return wishListRepository.findByLink(wishlistLink)
-                .orElseThrow(() -> new WishListException(NOT_FOUND_WISHLIST));
-    }
-
-    private static void checkFundingExist(WishList wishList) {
-        wishList.getProductList().stream()
-                .flatMap(product -> product.getFundingList().stream())
-                .filter(funding -> funding.getStatus() == Status.A)
-                .findAny()
-                .ifPresent(funding -> {
-                    throw new WishListException(NOT_DELETE_WISHLIST_BY_FUNDING);
-                });
-    }
+	private static void checkFundingExist(WishList wishList) {
+		wishList.getProductList().stream().flatMap(product -> product.getFundingList().stream())
+				.filter(funding -> funding.getStatus() == Status.A).findAny().ifPresent(funding -> {
+					throw new WishListException(NOT_DELETE_WISHLIST_BY_FUNDING);
+				});
+	}
 }
